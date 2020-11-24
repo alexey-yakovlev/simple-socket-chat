5e9e8ae9d9dcddce4ce2468f57b01480b748ae27
Alexei Yakovlev <morrigard@gmail.com>
2020-11-24 14:31:30 +0300

    add path

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const app = express();
 const server = require('http').createServer(app);
 const cors = require('cors');
