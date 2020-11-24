93ba0ef9238c1a4f0ab26b3386a905c5aee4a3e8
Alexei Yakovlev <morrigard@gmail.com>
2020-11-24 18:11:20 +0300

    edit PORT

--- client/src/config.js
+++ client/src/config.js
@@ -2,5 +2,5 @@
 const productionUrl = window.location.origin;
 const developmentUrl = 'http://localhost';
 export const HOST = process.env.NODE_ENV === 'production' ? productionUrl : developmentUrl;
-export const PORT = process.env.REACT_APP_PORT || process.env.PORT || 1234;
+export const PORT = process.env.REACT_APP_PORT || process.env.PORT || 80;
 // export const HOST = process.env.HEROKU_APP_NAME || 'http://localhost';
